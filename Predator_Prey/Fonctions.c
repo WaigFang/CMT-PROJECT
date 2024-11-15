a4ed449ccd5c5a6901bcd16341707a4f5489fa32
WaigFang <leo.waigfanghernandez@epfl.ch>
2024-11-15 11:14:08 +0100

    Project Code  README.md

--- Predator_Prey/Fonctions.c
+++ Predator_Prey/Fonctions.c
@@ -1,14 +1,12 @@
 #include <math.h>
 #include <stdio.h>
 
-void prey_density_over_time(double alpha,double beta,int x,int y){
-    double dx_dt = 0;
-    dx_dt = (alpha * x) - (beta * x *y);
+double prey_density_over_time(double alpha,double beta,int x,int y){
+    return (alpha * x) - (beta * x *y);
 }
 
 
-void predator_density_over_time(double delta, double gamma, int x, int y){
-double dy_dt = 0; 
-dy_dt = (delta*x*y) - (gamma*y);
+double predator_density_over_time(double delta, double gamma, int x, int y){
+    return (delta*x*y) - (gamma*y);
 }
 
