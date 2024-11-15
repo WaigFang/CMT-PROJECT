9dbc3f627b8484ccc0373bd38a5b39544bd9bb1a
WaigFang <leo.waigfanghernandez@epfl.ch>
2024-11-15 11:29:26 +0100

    modified:   Predator_Prey/Fonctions.c

--- Predator_Prey/Fonctions.c
+++ Predator_Prey/Fonctions.c
@@ -1,12 +1,11 @@
 #include <math.h>
 #include <stdio.h>
 
-double prey_density_over_time(double alpha,double beta,int x,int y){
+double prey_growth_rate(double alpha,double beta,int x,int y){ //dx/dt // alpha,beta > 0
     return (alpha * x) - (beta * x *y);
 }
 
 
-double predator_density_over_time(double delta, double gamma, int x, int y){
+double predator_growth_rate(double delta, double gamma, int x, int y){ // dy/dt //delta,gamma > 0
     return (delta*x*y) - (gamma*y);
 }
-
