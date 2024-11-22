c0769d7328836573e6b35f6feba5a43d9a433297
WaigFang <leo.waigfanghernandez@epfl.ch>
2024-11-22 12:03:47 +0100

    pruebas en main.py

--- Predator_Prey/main.py
+++ Predator_Prey/main.py
@@ -19,6 +19,25 @@ fonc.predator_growth_rate.argtypes = (ctypes.c_double,ctypes.c_double,ctypes.c_i
 fonc.predator_growth_rate.restype = ctypes.c_double
 
 
+class Timepopulation(ctypes.Structure):
+    _fields_ = [
+        ("t", ctypes.POINTER(ctypes.c_double)),
+        ("x", ctypes.POINTER(ctypes.c_double)),
+        ("y", ctypes.POINTER(ctypes.c_double))
+    ]
+fonc.population_evolution.restype = ctypes.POINTER(Timepopulation)
+fonc.population_evolution.argtypes = [
+    ctypes.c_int,  # maxtime
+    ctypes.c_double,  # alpha
+    ctypes.c_double,  # beta
+    ctypes.c_double,  # delta
+    ctypes.c_double,  # gamma
+    ctypes.c_double,  # x
+    ctypes.c_double   # y
+]
+fonc.free_population.argtypes = [ctypes.POINTER(Timepopulation)]
+fonc.free_population.restype = None
+
 file = open("Data/Leigh1968_harelynx.csv", "r")
 csvReader = csv.reader(file, delimiter = ',')
 Years = []
@@ -40,14 +59,45 @@ plt.plot(Years,Lynxs,label="Lynx Population",color="red")
 plt.plot(Years,Hares,label="Hare Popuation",color="blue")
 plt.legend()
 plt.show()
-#Plots both populations in function of time
-pruebas= []
-for i in range (len(Hares)):
-    prueba = fonc.prey_growth_rate(0.500,0.03,Hares[i],Lynxs[i])
-    pruebas.append(prueba)
 
-plt.plot(Years,pruebas)
-plt.title("Model Predicted Prey Growth Rate")
-plt.xlabel("Time (Years)")
-plt.ylabel("Predicted Prey Growth")
-plt.show() #look for errors as I do not understand what we are computing 
+
+
+
+#otra prueba mas
+result = fonc.population_evolution(56, 0.4, 0.5, 0.09, 2.0, 21000, 49000)
+if not result:
+    raise MemoryError("Failed to allocate memory in C code.")
+
+# Extract data from the C struct
+# time = np.array([result.contents.t[i] for i in range(56)])
+# prey = np.array([result.contents.x[i] for i in range(56)])
+# predator = np.array([result.contents.y[i] for i in range(56)])
+time = np.ctypeslib.as_array(result.contents.t, shape=(56,))
+prey = np.ctypeslib.as_array(result.contents.x, shape=(56,))
+predator = np.ctypeslib.as_array(result.contents.y, shape=(56,))
+print(time)
+print(prey)
+print(predator)
+fonc.free_population(result)
+plt.figure(figsize=(10, 6))
+plt.plot(time, prey, label="Prey Population")
+plt.plot(time, predator, label="Predator Population")
+plt.xlabel("Time")
+plt.ylabel("Population")
+plt.legend()
+plt.title("Population Evolution (Lotka-Volterra)")
+plt.grid()
+plt.show()
+
+
+#Plots both populations in function of time
+# pruebas= []
+# for i in range (len(Hares)):
+#     prueba = fonc.prey_growth_rate(0.500,0.03,Hares[i],Lynxs[i])
+#     pruebas.append(prueba)
+
+# plt.plot(Years,pruebas)
+# plt.title("Model Predicted Prey Growth Rate")
+# plt.xlabel("Time (Years)")
+# plt.ylabel("Predicted Prey Growth")
+# plt.show() #look for errors as I do not understand what we are computing 
